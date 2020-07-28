3317abf36c4b902cdcc65cfe9f112ff6a8b0d8cf
iswandii <andi.jr103@gmail.com>
2020-07-28 21:51:10 +0700

    membuat latihan pertama tentang logika php

---
+++ latihan1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan </title>
+</head>
+
+<body>
+    <?php
+    $uang = 60000;
+    $aqua = 3000;
+    $mie = 2500;
+    $saus = 5000;
+
+    $total = (2 * $aqua) + (3 * $mie) + (1 * $saus);
+    $kembalian = $uang - $total;
+    echo "Uang yang harus dibayar Budi adalah $total <br>";
+    echo "Kembalian yang diperoleh Budi adalah $kembalian"
+    ?>
+</body>
+
+</html>
