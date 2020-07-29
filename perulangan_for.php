9f41d120d3b3c065d282c5dda72762c69760b062
iswandii <andi.jr103@gmail.com>
2020-07-29 20:30:21 +0700

    menambahkan beberapa contoh for

--- perulangan_for.php
+++ perulangan_for.php
@@ -13,6 +13,34 @@
     for ($i = 1; $i < 5; $i++) {
         echo "Are u okay ? <br>";
     }
+    //  ! Contoh 1 
+    for ($i = 0; $i <= 10; $i++) {
+        echo $i;
+    }
+    echo "<br>";
+
+    // ! Contoh 2
+    for ($i = 1;; $i++) {
+        if ($i > 10) {
+            break;
+        }
+        echo $i;
+    }
+    echo "<br>";
+
+    // ! Contoh 3 
+    $i = 1;
+    for (;;) {
+        if ($i > 10) {
+            break;
+        }
+        echo "$i";
+        $i++;
+    }
+    echo "<br><br>";
+
+    // ! Contoh 4 
+    for ($i = 1; $i <= 10; print "$i", $i++);
     ?>
 </body>
 
