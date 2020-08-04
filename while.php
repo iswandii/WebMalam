d3a648745114d572152f0ef6180548f76cb38425
iswandii <andi.jr103@gmail.com>
2020-08-04 17:24:49 +0700

    membuat contoh while

---
+++ while.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>While</title>
+</head>
+
+<body>
+    <?php
+    $a = 1;
+    while ($a <= 10) {
+        echo $a;
+        $a++;
+    }
+
+    $b = 10;
+    while ($b > -10) {
+        echo $b;
+        $b--;
+    }
+    ?>
+</body>
+
+</html>
