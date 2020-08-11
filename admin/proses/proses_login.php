5741278a219fcf1cb6565cf70d6583cefef5e478
iswandii <andi.jr103@gmail.com>
2020-08-11 21:24:14 +0700

    membuat koneksi dan proses_login

---
+++ admin/proses/proses_login.php
@@ -0,0 +1,17 @@
+<?php
+include('koneksi.php');
+if (isset($_POST['login'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $query = mysqli_query($koneksi, "SELECT * FROM user WHERE username ='$username' AND password='$password'");
+    $cek = mysqli_num_rows($query);
+
+    if ($cek > 0) {
+        session_start();
+        $_SESSION['username'] = $username;
+        header('location:../index.php');
+    } else {
+        header('location:../login.php');
+    }
+}
