4aa1d30b01dd48108d5155da76c917bcc17733be
iswandii <andi.jr103@gmail.com>
2020-08-12 16:57:01 +0700

    membuat beberapa form

--- admin/register.php
+++ admin/register.php
@@ -28,10 +28,10 @@
           <div class="col-lg-4 mx-auto">
             <h2 class="text-center mb-4">Register</h2>
             <div class="auto-form-wrapper">
-              <form action="#">
+              <form action="proses/proses_regis.php" method="POST">
                 <div class="form-group">
                   <div class="input-group">
-                    <input type="text" class="form-control" placeholder="Username">
+                    <input type="text" class="form-control" placeholder="Username" name="username">
                     <div class="input-group-append">
                       <span class="input-group-text">
                         <i class="mdi mdi-check-circle-outline"></i>
@@ -39,9 +39,10 @@
                     </div>
                   </div>
                 </div>
+
                 <div class="form-group">
                   <div class="input-group">
-                    <input type="password" class="form-control" placeholder="Password">
+                    <input type="password" class="form-control" placeholder="Password" name="password">
                     <div class="input-group-append">
                       <span class="input-group-text">
                         <i class="mdi mdi-check-circle-outline"></i>
@@ -49,9 +50,10 @@
                     </div>
                   </div>
                 </div>
+
                 <div class="form-group">
                   <div class="input-group">
-                    <input type="password" class="form-control" placeholder="Confirm Password">
+                    <input type="password" class="form-control" placeholder="Confirm Password" name="password">
                     <div class="input-group-append">
                       <span class="input-group-text">
                         <i class="mdi mdi-check-circle-outline"></i>
@@ -59,19 +61,42 @@
                     </div>
                   </div>
                 </div>
+
+                <div class="form-group">
+                  <div class="input-group">
+                    <input type="email" class="form-control" placeholder="email@email" name="email">
+                    <div class="input-group-append">
+                      <span class="input-group-text">
+                        <i class="mdi mdi-check-circle-outline"></i>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="form-group">
+                  <div class="input-group">
+                    <input type="text" class="form-control" placeholder="0853********" name="no_hp">
+                    <div class="input-group-append">
+                      <span class="input-group-text">
+                        <i class="mdi mdi-check-circle-outline"></i>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+
                 <div class="form-group d-flex justify-content-center">
                   <div class="form-check form-check-flat mt-0">
                     <label class="form-check-label">
-                      <input type="checkbox" class="form-check-input" checked> I agree to the terms
+                      <input type="checkbox" class="form-check-input" checked name="persetujuan"> Saya setuju dengan S&K yang berlaku.
                     </label>
                   </div>
                 </div>
                 <div class="form-group">
-                  <button class="btn btn-primary submit-btn btn-block">Register</button>
+                  <button class="btn btn-primary submit-btn btn-block" name="regis" type="submit">Register</button>
                 </div>
                 <div class="text-block text-center my-3">
                   <span class="text-small font-weight-semibold">Already have and account ?</span>
-                  <a href="login.html" class="text-black text-small">Login</a>
+                  <a href="login.php" class="text-black text-small">Login</a>
                 </div>
               </form>
             </div>
